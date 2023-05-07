7f7c3ea212c04fdc8d5f9e9b55f6ed1287274980
Nils Frielinghaus <nils.frielinghaus@sqior.com>
2023-05-07 22:21:07 +0200

    Basic app styling with input control

--- phonebook/src/App.tsx
+++ phonebook/src/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import { PhonebookReferenceImplementation } from './Phonebook';
 import { PhonebookSamples } from './PhonebookSamples';
@@ -12,18 +11,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className='input-container'>
+          <input type='text' autoFocus className="input" placeholder='Type to search for phonebook entries'/>
+        </div>
       </header>
     </div>
   );
